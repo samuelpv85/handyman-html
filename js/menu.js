248d7d9a2a31debdff8b9ff2d56a8dd8ffa94da9
Samuel Patiño <levivo2@hotmail.com>
2025-02-25 02:43:53 -0300

    Mejoras reponsive CSS

--- js/menu.js
+++ js/menu.js
@@ -3,21 +3,27 @@ document.addEventListener("DOMContentLoaded", function () {
   const navLinks = document.querySelector(".nav-links");
   const links = document.querySelectorAll(".nav_link");
 
+  // Toggle menú
   menuToggle.addEventListener("click", function () {
     navLinks.classList.toggle("active");
+    document.body.style.overflow = navLinks.classList.contains("active")
+      ? "hidden"
+      : "";
   });
 
   // Cerrar menú al hacer click en un enlace
   links.forEach((link) => {
     link.addEventListener("click", () => {
       navLinks.classList.remove("active");
+      document.body.style.overflow = "";
     });
   });
 
-  // Cerrar menú al hacer click fuera
-  document.addEventListener("click", (e) => {
-    if (!navLinks.contains(e.target) && !menuToggle.contains(e.target)) {
+  // Ajustar menú en resize
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 768) {
       navLinks.classList.remove("active");
+      document.body.style.overflow = "";
     }
   });
 });
